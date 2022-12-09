3c3687e14449db137c1345daf8881ceb01e4d7b7
Sayali255 <111580642+Sayali255@users.noreply.github.com>
2022-12-09 17:56:27 +0530

    commit

---
+++ assets/js/app.js
@@ -0,0 +1,51 @@
+// <!--  // NavBar Start//  -->
+// <header>
+//     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+
+//         <a class="navbar-brand" href="index.html">
+            
+//             <img class="w-100" src="https://www.edigitalagency.com.au/wp-content/uploads/Twitter-logo-png.png" alt="Pintrest" title="Pintrest">
+
+//         </a>
+
+//         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+//           <span class="navbar-toggler-icon"></span>
+//         </button>
+      
+//         <div class="collapse navbar-collapse animation" id="navbarSupportedContent">
+//             <div class="collapse navbar-collapse" id="navbarSupportedContent">
+//                 <ul class="navbar-nav ml-auto">
+//                   <li class="nav-item active">
+//                     <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+//                   </li>
+//                   <li class="nav-item">
+//                     <a class="nav-link" href="#">about us</a>
+//                   </li>
+
+//                   <li class="nav-item">
+//                     <a class="nav-link" href="#">services</a>
+//                   </li>
+
+//                   <li class="nav-item">
+//                     <a class="nav-link" href="#">blogs</a>
+//                   </li>
+
+//                   <li class="nav-item">
+//                     <a class="nav-link" href="#">contact-us</a>
+//                   </li>
+
+
+//                  </ul>
+//         </div>
+//       </nav>
+// </header>
+
+
+
+
+
+
+
+
+
+
